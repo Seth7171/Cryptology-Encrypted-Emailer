bfae5e6d39a65c49c7bc20115435d644e8676c29
lior <audiblemaple@gmail.com>
2024-03-11 10:28:20 +0200

    added elgamal PRIME N generation

--- EccElGamal/elgamal_class.py
+++ EccElGamal/elgamal_class.py
@@ -1,11 +1,12 @@
 import random
-
 import env
+from sympy import nextprime
 
 
 class ElgamalEncryption:
     def __init__(self, n=None, base_point_x=None, base_point_y=None, end_point_x=None, end_point_y=None):
-        self.n = n if n is not None else random.randint(5000, 9999)
+        # self.n = n if n is not None else random.randint(5000, 9999)
+        self.n = n if n is not None else self.get_random_prime()
         self.a = random.randint(10, 9999)
         self.b = random.randint(10, 9999)
         self.LHS = [[], []]
@@ -79,6 +80,19 @@ class ElgamalEncryption:
         if env.debug:
             print(f"Decrypted message received by receiver is: {decrypted_message}")
         return decrypted_message
+
+
+    def get_random_prime(self, lower_bound=314, upper_bound=9999):
+        random_number = random.randint(lower_bound, upper_bound)
+        random_prime = nextprime(random_number)
+
+        # Ensure the prime number is within the upper bound
+        if random_prime > upper_bound:
+            # If the generated prime is greater than upper_bound, find the previous prime before upper_bound
+            random_prime = nextprime(upper_bound, ith=-1)
+
+        return random_prime
+
 
 
 # Example usage
